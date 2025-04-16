912ab990360e8a6ba6994a7cba10b5a1866ee110
hbessette <haydenbessette@gmail.com>
2025-04-16 19:24:31 -0400

    a6

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,8 +1,10 @@
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function AccountNavigation() {
-  const { currentUser } = useSelector((state: any) => state.accountReducer);
+   const { currentUser } = useSelector((state: any) => state.accountReducer);
+   const { pathname } = useLocation();
+   const active = (path: string) => (pathname.includes(path) ? "active text-black" : "text-danger");
 
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
@@ -10,13 +12,17 @@ export default function AccountNavigation() {
         <>
           <Link
             to={"/Kambaz/Account/Signin"}
-            className="list-group-item text-black active border border-0"
+            className={`list-group-item text-danger border border-0 ${active(
+              "Signin"
+            )}`}
           >
             Signin
           </Link>
           <Link
             to={"/Kambaz/Account/Signup"}
-            className="list-group-item text-danger border border-0"
+            className={`list-group-item text-danger border border-0 ${active(
+              "Signup"
+            )}`}
           >
             Signup
           </Link>
@@ -25,9 +31,22 @@ export default function AccountNavigation() {
       {currentUser && (
         <Link
           to={"/Kambaz/Account/Profile"}
-          className="list-group-item text-danger border border-0"
+          className={`list-group-item text-danger border border-0 ${active(
+            "Profile"
+          )}`}
         >
           Profile
+        </Link>
+      )}
+      {currentUser && currentUser.role === "ADMIN" && (
+        <Link
+          to={`/Kambaz/Account/Users`}
+          className={`list-group-item text-danger border border-0 ${active(
+            "Users"
+          )}`}
+        >
+          {" "}
+          Users{" "}
         </Link>
       )}
     </div>
--- src/Kambaz/Courses/People/Table.tsx
+++ src/Kambaz/Courses/People/Table.tsx
@@ -1,13 +1,12 @@
 
-import { useParams } from "react-router-dom";
-import * as db from "../../Database";
 import { Table } from "react-bootstrap";
+import PeopleDetails from "./Details";
 import { FaUserCircle } from "react-icons/fa";
-export default function PeopleTable() {
-  const { cid } = useParams();
-  const { users, enrollments } = db;
+import { Link } from "react-router-dom";
+export default function PeopleTable({ users = [], fetchUsers} : { users? : any[], fetchUsers : any}) {
   return (
     <div id="wd-people-table">
+      <PeopleDetails fetchUsers={fetchUsers} />
       <Table striped>
         <thead>
           <tr>
@@ -20,27 +19,25 @@ export default function PeopleTable() {
           </tr>
         </thead>
         <tbody>
-          {users
-            .filter((user) =>
-              enrollments.some(
-                (enrollment) =>
-                  enrollment.user === user._id && enrollment.course === cid
-              )
-            )
-            .map((user: any) => (
-              <tr key={user._id}>
-                <td className="wd-full-name text-nowrap">
+          {users.map((user: any) => (
+            <tr key={user._id}>
+              <td className="wd-full-name text-nowrap">
+                <Link
+                  to={`/Kambaz/Account/Users/${user._id}`}
+                  className="text-decoration-none"
+                >
                   <FaUserCircle className="me-2 fs-1 text-secondary" />
                   <span className="wd-first-name">{user.firstName} </span>
                   <span className="wd-last-name">{user.lastName}</span>
-                </td>
-                <td className="wd-login-id">{user.loginId}</td>
-                <td className="wd-section">{user.section}</td>
-                <td className="wd-role">{user.role}</td>
-                <td className="wd-last-activity">{user.lastActivity}</td>
-                <td className="wd-total-activity">{user.totalActivity}</td>
-              </tr>
-            ))}
+                </Link>
+              </td>
+              <td className="wd-login-id">{user.loginId}</td>
+              <td className="wd-section">{user.section}</td>
+              <td className="wd-role">{user.role}</td>
+              <td className="wd-last-activity">{user.lastActivity}</td>
+              <td className="wd-total-activity">{user.totalActivity}</td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     </div>
